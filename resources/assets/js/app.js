7cca29e0d00d42705962492088c930b63426117f
tw <t888y@qq.com>
2018-01-20 03:15:49 +0800

    初始化前端项目

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,21 @@
+import Vue from 'vue';
+import tHttp from './utils/tHttp';
+import router from './router';
+import App from './App.vue';
+import 'normalize.css';
+import { getBaseUrl } from './utils/utils';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// 获取baseUrl
+let baseUrl = getBaseUrl();
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+Vue.use(tHttp, {
+  baseURL: baseUrl + 'api/',
+  router
 });
+
+new Vue({
+  el: '#app',
+  ...App,
+  router
+});
+
