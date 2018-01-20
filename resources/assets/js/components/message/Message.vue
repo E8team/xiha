be6e8d4eb8874e087c67b85a2804e23ea1d9683f
tw <t888y@qq.com>
2018-01-20 23:19:47 +0800

    Meessage 和 Confirm组件

---
+++ resources/assets/js/components/message/Message.vue
@@ -0,0 +1,67 @@
+<template>
+  <transition name="el-message-fade">
+    <div v-show="visable" class="message" :class="type">
+      {{msg}}
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visable: false,
+      type: 'error',
+      msg: '',
+      delay: 3000
+    };
+  },
+  methods: {
+    destroyElement () {
+      this.$el.removeEventListener('transitionend', this.destroyElement);
+      this.$el.parentNode.removeChild(this.$el);
+      this.$destroy(true);
+    }
+  },
+  mounted () {
+    this.visable = true;
+    setTimeout(() => {
+      this.$el.addEventListener('transitionend', this.destroyElement);
+      this.visable = false;
+    }, this.delay);
+  }
+};
+</script>
+
+<style scoped>
+  .message{
+    line-height: 28px;
+    font-size: 12px;
+    color: #fff;
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: #333;
+    text-align: center;
+    transition: opacity .3s,transform .4s;
+  }
+  .message.error{
+    color: #a94442;
+    background-color: #f2dede;
+  }
+  .message.info{
+    color: #31708f;
+    background-color: #d9edf7;
+  }
+  .message.success{
+    color: #3c763d;
+    background-color: #dff0d8;
+  }
+  .el-message-fade-enter,.el-message-fade-leave-active{
+    opacity:0;
+    -webkit-transform:translate(0, -100%);
+    transform:translate(0, -100%)
+  }
+</style>
