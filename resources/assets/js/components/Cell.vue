7895d4892020a22ac7eaab78c36c9039c19c074b
tw <t888y@qq.com>
2018-01-22 03:59:25 +0800

    修改个人资料页面

---
+++ resources/assets/js/components/Cell.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cell">
+    <div class="title">
+      {{title}}
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cell',
+  props: {
+    title: String
+  }
+};
+</script>
+<style lang="less" scoped>
+.cell{
+  background-color: #fff;
+  padding: 15px 10px;
+  border-bottom: 1px solid #eee;
+  overflow: hidden;
+  position: relative;
+  min-height: 49.4px;
+  .title, .content{
+    font-size: 16px;
+  }
+  .title{
+    text-align: left;
+    color: #505050;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .content{
+    float: right;
+    color: #999;
+  }
+}
+</style>
+
--- resources/assets/js/views/UserSettings.vue
+++ resources/assets/js/views/UserSettings.vue
@@ -1,3 +1,43 @@
 <template>
-  <div>编辑个人中心</div>  
+  <div class="user_settings">
+    <TNav></TNav>
+    <div class="items">
+      <Cell title="头像"><img class="cover" :src="me.avatar && me.avatar.cover_url" :alt="me.name"></Cell>
+      <Cell title="昵称">{{me.name}}</Cell>
+      <Cell title="用户名">{{me.username}}</Cell>
+    </div>
+    <button class="logout_btn">退出登录</button>
+  </div>
 </template>
+<script>
+import Cell from '../components/Cell'
+import TNav from '../components/TNav';
+export default {
+  components: { TNav, Cell },
+  computed: {
+    me () {
+      return this.$store.state.me || {};
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.items{
+  padding-top: 10px;
+  .cover{
+    width: 70px;
+    height: 70px;
+  }
+}
+.logout_btn{
+  margin-top: 30px;
+  background-color: #fff;
+  font-size: 16px;
+  border: 0;
+  width: 100%;
+  padding: 15px 0;
+  color: #666;
+}
+</style>
+
