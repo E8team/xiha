066d9dd834c3196c1ca492a42285d0754283009f
taoyu <1255803549@qq.com>
2018-01-21 15:33:59 +0800

    up

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -9,6 +9,10 @@ class Image extends BaseModel
 
     protected $primaryKey = 'hash';
 
+    protected $keyType = 'char';
+
+    public $incrementing = false;
+
     public function getUrl($style = null)
     {
         return image_url($this->hash . '.' . $this->ext, $style);
