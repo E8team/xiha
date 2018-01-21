a9d952e9c96a625ca165403d84f7d70af86219ba
taoyu <1255803549@qq.com>
2018-01-21 18:23:45 +0800

    up

---
+++ app/Listeners/UpdateVoteCount.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+
+use App\Events\Voted;
+
+class UpdateVoteCount
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  $event
+     * @return void
+     */
+    public function handle(Voted $event)
+    {
+
+    }
+}
