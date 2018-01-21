cc3cf914d38402bb97a3922e75fce0846ff514c0
tw <t888y@qq.com>
2018-01-21 16:35:17 +0800

    显示笑话详情

--- resources/assets/js/components/JokeBody.vue
+++ resources/assets/js/components/JokeBody.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="joke">
     <header>
-      <img class="user_img" src="http://image.uc.cn/s/uae/g/0q/avatar/43.png">
-      <div class="user_name">金牌港C</div>
+      <template @click="$router.push({name: 'user', params: {id: joke.user.username}})">
+        <img class="user_img" :src="joke.user && joke.user.avatar.cover_url">
+        <div class="user_name">{{joke.user && joke.user.name}}</div>
+      </template>
     </header>
     <section class="content">
-      你若军训 便是晴天，你若放假 便是雨天，你若发奋做作业 便是开学前一天，多么痛的领悟 。
+      {{joke.content}}
     </section>
     <footer>
       <button class="btn vote" type="button">
@@ -24,11 +26,19 @@
           <path d="M10.241 19.313a.97.97 0 0 0-.77.2 7.908 7.908 0 0 1-3.772 1.482.409.409 0 0 1-.38-.637 5.825 5.825 0 0 0 1.11-2.237.605.605 0 0 0-.227-.59A7.935 7.935 0 0 1 3 11.25C3 6.7 7.03 3 12 3s9 3.7 9 8.25-4.373 9.108-10.759 8.063z" fill-rule="evenodd">
           </path>
         </svg>
-        <div>评论 10</div>
+        <div>评论 {{joke.comments_count}}</div>
       </button>
     </footer>
   </div>
 </template>
+<script>
+export default {
+  name: 'JokeBody',
+  props: {
+    joke: Object
+  }
+};
+</script>
 
 <style lang="less" scoped>
 .joke{
